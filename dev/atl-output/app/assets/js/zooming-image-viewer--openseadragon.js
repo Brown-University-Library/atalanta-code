4cd14155f8bec7fa409b6d9538bdf53d6d73e86b
cbrusch <crystal_brusch@brown.edu>
2018-09-07 11:14:27 -0400

    switching zooming image tiles from hosted W scans to local B scans

--- dev/atl-output/app/assets/js/zooming-image-viewer--openseadragon.js
+++ dev/atl-output/app/assets/js/zooming-image-viewer--openseadragon.js
@@ -2,7 +2,6 @@ $(function () {
 	var myEmblemDataNum = $('.emblem-page').data("id"); // get the data ID for the current emblem page
 	var startPage // the number of first page of current emblem
 	var pageTiles = "../data/pageView.json"; // file path to page view dzi files
-	console.log("This is pageTiles " + pageTiles);
 	var bookTiles = "../data/bookView.json"; // file path to book view dzi files
 	var pageView = $.getJSON(pageTiles, function(myJSON) { // get pageView.json file
 		pageView = pageView.responseJSON; // get array of page view URLS
@@ -11,18 +10,22 @@ $(function () {
 		bookView = bookView.responseJSON; // get array of book view URLS
 	})
 	.done(function() { // after all the image tiles are ready, display zoomable pages
-		if (myEmblemDataNum <= 4) { // handle front matter (not in sets of 4)
-			startPage = myEmblemDataNum + 3;
+		if (myEmblemDataNum <= 3) { // handle front matter (not in sets of 4)
+			startPage = myEmblemDataNum + 6;
+		}
+		else if (myEmblemDataNum === 4) { // handle front matter: preface
+			startPage = myEmblemDataNum + 8;
 		}
 		else if (myEmblemDataNum > 4) { // handle emblems (in sets of 4)
-			startPage = myEmblemDataNum * 4 - 7;
+			startPage = myEmblemDataNum * 4 - 2;
 		}
 		var atalantaZoom = pageView; // current view mode (initially pageView)
 		var viewer = OpenSeadragon({
 			id: "openseadragon-wrapper",
 			// tileSources: [atalantaZoom],
 			tileSources: [atalantaZoom],
-			initialPage: startPage, // start viewer at first page of current emblem
+			// initialPage: startPage, // start viewer at first page of current emblem
+			// tileSources: "../assets/img/emblem-images_tiled/page-view/pageView14.dzi",
 			autoResize: true, /***/
             showHomeControl: false, /***/
 			animationTime: 1.5, /* smoother zooming with easing */
@@ -38,6 +41,7 @@ $(function () {
 			previousButton: "previous",
 			nextButton: "next"
 		});
+		console.log(viewer);
 		
 		// var pageIndex = this.pageIndex - (this.mode === 'book' ? 2 : 1);
 		// 	if (this.mode === 'book')
