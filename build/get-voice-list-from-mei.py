80c889a4a2bf3edbe652b71adbf78654abe843fa
Patrick Rashleigh <edward_rashleigh@brown.edu>
2018-11-07 18:56:23 -0500

    Fixed order of voices in emblem fragments

---
+++ build/get-voice-list-from-mei.py
@@ -0,0 +1,26 @@
+from glob import glob
+import re
+
+# This script scans the MEI files for the fugues and
+#  generates a python list-of-lists to be pasted into
+#  generate-atalanta-music-html.py
+
+extractFugueNumber_RE = re.compile(r'/Fuga\s(\d+)\.xml$')
+extractVoiceNames_RE = re.compile(r'<staffDef\s+([^>]+\s+)?label="([^"]+)"')
+
+allVoiceInitials = []
+
+for filename in glob('/Users/patrickrashleigh/Documents/Atalanta/atalanta-code/data/mei/*.xml'):
+  fugueNumber = re.search(extractFugueNumber_RE, filename).group(1)
+  file = open(filename, encoding='utf-16')
+  mei = file.read()
+  staffDefMatches = re.findall(extractVoiceNames_RE, mei)
+  voiceInitials = []
+  for staffDefMatch in staffDefMatches:
+    print(staffDefMatch[1][0])
+    voiceInitials.append("'" + staffDefMatch[1][0] + "'")
+  allVoiceInitials.append('[' + ','.join(voiceInitials) + ']')
+
+
+print('[ False, ' + ','.join(allVoiceInitials) + ']')
+
