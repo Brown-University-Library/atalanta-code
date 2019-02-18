0779ea23c729cc58beed462958c7ef151383fa33
cbrusch <crystal_brusch@brown.edu>
2019-02-18 16:56:46 -0500

    fixing search modal design on mobile

--- dev/atl-output/app/assets/js/topnav.js
+++ dev/atl-output/app/assets/js/topnav.js
@@ -7,7 +7,7 @@ $(function () {
 	var animationMenuSlideIn = 'topnav--slide-in';
 	var animationMenuSlideOut = 'topnav--slide-out';
 	var topnavSearchBtn = '.topnav__search button';
-	var searchModalOpen = 'topnav__search--open';
+	var searchModalOpened = 'topnav__search--open';
 	var searchModalClosed = 'topnav__search--closed';
 	var searchModal = 'div.search__modal';
 	var animationSearchSlideIn = 'search__modal--slide-in';
@@ -17,24 +17,18 @@ $(function () {
 	var searchURL = '../search/search.html?q='
 	var searchQueryInput = 'input#search__bar__field';
 	var submitSearchBtn = '.search__modal button.submit';
-	var $documentElement = $('html, body'),
-		$wrapper = $('.main'),
-		scrollTop;
+	var $documentElement = $('html, body');
+	var	$wrapper = $('.main');
+	var	scrollTop;
 
 /* EVENTS */
 	/* topnav mobile menu */
 	$(hamburgerMenuBtn).click(function() {
 		if($(hamburgerMenuBtn).hasClass(hamburgerMenuBtnClosed)) {
-			$(hamburgerMenuBtn).removeClass(hamburgerMenuBtnClosed);
-			$(hamburgerMenuBtn).addClass(hamburgerMenuBtnOpen);
-			$(hamburgerMenu).removeClass(animationMenuSlideOut);
-			$(hamburgerMenu).addClass(animationMenuSlideIn);
+			hamburgerMenuOpen();
 		}
 		else if($(hamburgerMenuBtn).hasClass(hamburgerMenuBtnOpen)) {
-			$(hamburgerMenuBtn).removeClass(hamburgerMenuBtnOpen);
-			$(hamburgerMenuBtn).addClass(hamburgerMenuBtnClosed);
-			$(hamburgerMenu).removeClass(animationMenuSlideIn);
-			$(hamburgerMenu).addClass(animationMenuSlideOut);
+			hamburgerMenuClose();
 		}
 		else {
 			console.log("THE HAMBURGER MENU HAS NO CLASS");
@@ -44,23 +38,11 @@ $(function () {
 	/* search topnav button */
 	$(topnavSearchBtn).click(function(event) {
 		if( $(topnavSearchBtn).hasClass(searchModalClosed) ) {
-			clearSearchText();
-			$(topnavSearchBtn).removeClass(searchModalClosed);
-			$(topnavSearchBtn).addClass(searchModalOpen);
-			$(searchModal).attr('aria-hidden', 'false');
-			$(searchModal).addClass(animationSearchSlideIn);
-			$(searchModal).removeClass(animationSearchSlideOut);
-			$('body').addClass('no-scroll');
-			lockBody();
+			searchTextClear();
+			searchModalOpen();
 		}
-		else if( $(topnavSearchBtn).hasClass(searchModalOpen) ) {
-			$(topnavSearchBtn).removeClass(searchModalOpen);
-			$(topnavSearchBtn).addClass(searchModalClosed);
-			$(searchModal).attr('aria-hidden', 'true');
-			$(searchModal).addClass(animationSearchSlideOut);
-			$(searchModal).removeClass(animationSearchSlideIn);
-			$('body').removeClass('no-scroll');
-			unlockBody();
+		else if( $(topnavSearchBtn).hasClass(searchModalOpened) ) {
+			searchModalClose();
 		}
 		else {
 			console.log("THE SEARCH BUTTON HAS NO STATE");
@@ -72,31 +54,11 @@ $(function () {
 		e.preventDefault();
 	});
 	$(xCloseBtn).click(function() {
-		console.log("I am clicking the search close button");
-		$(topnavSearchBtn).removeClass(searchModalOpen);
-		$(topnavSearchBtn).addClass(searchModalClosed);
-		$(searchModal).attr('aria-hidden', 'true');
-		$(searchModal).addClass(animationSearchSlideOut);
-		$(searchModal).removeClass(animationSearchSlideIn);
-		$('body').removeClass('no-scroll');
-		unlockBody();
+		searchModalClose();
 	});
-	/* clear search text field on click/focus */
-	// $('input:text').focus(function() {
-	// 	$(this).val('');
-	// });
-	// $('input:text').focus(function() {
-	// 	clearSearchText();
-	// });
 	$(document).keydown(function(event) {
 		if(event.keyCode == 27) {
-			$(topnavSearchBtn).removeClass(searchModalOpen);
-			$(topnavSearchBtn).addClass(searchModalClosed);
-			$(searchModal).attr('aria-hidden', 'true');
-			$(searchModal).addClass(animationSearchSlideOut);
-			$(searchModal).removeClass(animationSearchSlideIn);
-			$('body').removeClass('no-scroll');
-			unlockBody();
+			searchModalClose();
 		}
 	});
 	/* submit on enter */
@@ -114,7 +76,7 @@ $(function () {
 /* FUNCTIONS */
 	/* prevent body scroll behind modal on iOS */
 	/* https://codepen.io/jerrylow/pen/yJeyoG */
-	function lockBody() {
+	function bodyLock() {
 		if(window.pageYOffset) {
 			scrollTop = window.pageYOffset;
 			$wrapper.css({
@@ -126,7 +88,7 @@ $(function () {
 			overflow: "hidden"
 		});
 	}
-	function unlockBody() {
+	function bodyUnlock() {
 		$documentElement.css({
 			height: "",
 			overflow: ""
@@ -139,14 +101,43 @@ $(function () {
 			scrollTop = null;
 		}, 0);
 	}
+	function hamburgerMenuClose() {
+		$(hamburgerMenuBtn).removeClass(hamburgerMenuBtnOpen);
+		$(hamburgerMenuBtn).addClass(hamburgerMenuBtnClosed);
+		$(hamburgerMenu).removeClass(animationMenuSlideIn);
+		$(hamburgerMenu).addClass(animationMenuSlideOut);
+	}
+	function hamburgerMenuOpen() {
+		$(hamburgerMenuBtn).removeClass(hamburgerMenuBtnClosed);
+		$(hamburgerMenuBtn).addClass(hamburgerMenuBtnOpen);
+		$(hamburgerMenu).removeClass(animationMenuSlideOut);
+		$(hamburgerMenu).addClass(animationMenuSlideIn);
+	}
+	function searchModalClose() {
+		$(topnavSearchBtn).removeClass(searchModalOpened);
+		$(topnavSearchBtn).addClass(searchModalClosed);
+		$(searchModal).attr('aria-hidden', 'true');
+		$(searchModal).addClass(animationSearchSlideOut);
+		$(searchModal).removeClass(animationSearchSlideIn);
+		$('body').removeClass('no-scroll');
+		bodyUnlock();
+	}
+	function searchModalOpen() {
+		$(topnavSearchBtn).removeClass(searchModalClosed);
+		$(topnavSearchBtn).addClass(searchModalOpened);
+		$(searchModal).attr('aria-hidden', 'false');
+		$(searchModal).addClass(animationSearchSlideIn);
+		$(searchModal).removeClass(animationSearchSlideOut);
+		$('body').addClass('no-scroll');
+		bodyLock();
+	}
+	/* clear text from search input field */
+	function searchTextClear() {
+		$(searchQueryInput).val('');
+	}
 	function updateURLwithSearchQuery() {
 		var searchInput = $(searchQueryInput).val();
 		var searchQueryURL =  searchURL + searchInput; 
 		window.open(searchQueryURL, '_self');
-	}
-	/* clear text from search input field */
-	function clearSearchText() {
-		console.log($(searchQueryInput).val());
-		$(searchQueryInput).val('');
 	}
 });
