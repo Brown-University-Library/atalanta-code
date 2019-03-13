f7290a4e4cfd0e21d60d9b8797f94dd377c4c329
cbrusch <crystal_brusch@brown.edu>
2019-03-13 16:22:41 -0400

    simplified detecting which category is selected, added comments

--- dev/atl-output/app/assets/js/image-search.js
+++ dev/atl-output/app/assets/js/image-search.js
@@ -15,62 +15,32 @@ $(function () {
 
 /* EVENTS */
 	$(imageCategoryTrigger).click(function() {
-		var that = this;
-		checkCategoryState(that);
+		var that = this; // store which category link was clicked
+		checkCategorySelected(that);
 	});
 	$(resultsTermsTrigger).click(function() {
-		// console.log("I CLICKED THE TERMS BUTTON");
-		// console.log(resultsItemTermsTrigger);
-		var that = this;
-		var currentItem = $(that).parent().parent().parent();
-		// console.log(currentItem);
-		var currentItemTerms = currentItem.find(resultsTermsContainer);
-		console.log(currentItemTerms);
-		$(currentItemTerms).addClass(resultsTermsRevealed);
+		var that = this; // store which results item terms button was clicked
+		resultsItemTermsOpen(that);
 	});
 /* FUNCTIONS */
-	function checkCategoryState(selectedCategory) {
-		if ( $(selectedCategory).parent().hasClass(imageCategory1) ) {
-			console.log("I CLICKED CATEGORY 1!");
-			revealSearchTerms(selectedCategory);
-		}
-		else if ( $(selectedCategory).parent().hasClass(imageCategory2) ) {
-			console.log("I CLICKED CATEGORY 2!");
-			revealSearchTerms(selectedCategory);
-		}
-		else if ( $(selectedCategory).parent().hasClass(imageCategory3) ) {
-			console.log("I CLICKED CATEGORY 3!");
-			revealSearchTerms(selectedCategory);
-		}
-		else if ( $(selectedCategory).parent().hasClass(imageCategory4) ) {
-			console.log("I CLICKED CATEGORY 4!");
-			revealSearchTerms(selectedCategory);
-		}
-		else if ( $(selectedCategory).parent().hasClass(imageCategory5) ) {
-			console.log("I CLICKED CATEGORY 5!");
-			revealSearchTerms(selectedCategory);
-		}
-		else if ( $(selectedCategory).parent().hasClass(imageCategory6) ) {
-			console.log("I CLICKED CATEGORY 6!");
-			revealSearchTerms(selectedCategory);
-		}
-		else if ( $(selectedCategory).parent().hasClass(imageCategory7) ) {
-			console.log("I CLICKED CATEGORY 7!");
-			revealSearchTerms(selectedCategory);
-		}
-		else {
-			console.log("NO CATEGORY IS SELECTED");
-		}
+	function checkCategorySelected(selectedCategory) {
+		var currentCategory = $(selectedCategory).parent(); // store which category is selected
+		// console.log(currentCategory);
+		revealSearchTerms(currentCategory);
 	}
 	function resultsItemTermsClose() {
 		
 	}
-	function resultsItemTermsOpen() {
-
+	function resultsItemTermsOpen(selectedResultsItem) {
+		var currentItem = $(selectedResultsItem).parent().parent().parent();
+		// console.log(currentItem);
+		var currentItemTerms = currentItem.find(resultsTermsContainer);
+		// console.log(currentItemTerms);
+		$(currentItemTerms).addClass(resultsTermsRevealed);
 	}
 	function revealSearchTerms(activeCategory) {
-		$(activeCategory).parent().siblings().removeClass('category--active');
-		$(activeCategory).parent().addClass('category--active');
+		$(activeCategory).siblings().removeClass('category--active'); // hide the last active category
+		$(activeCategory).addClass('category--active'); // reveal the current selected category
 	}
 
 });
