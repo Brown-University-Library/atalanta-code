73c206086a9a7c30fb78deebf73a3265451bb634
cbrusch <crystal_brusch@brown.edu>
2019-03-19 12:54:03 -0400

    adding all 51 image items to the grid

--- dev/atl-output/app/assets/js/image-search.js
+++ dev/atl-output/app/assets/js/image-search.js
@@ -1,7 +1,17 @@
 $(function () {
 /* VARIABLES */
+	// var mustache = require("mustache");
+	// var html = mustache.to_html(imageItemTemplate, imageItem);
+	// var imageItem = {
+	// 	imageArrayNum: "0",
+	// 	imageSrc: "../assets/img/emblem-images_cropped/320/emblem00.320.jpg",
+	// 	imageLabel: "Frontispiece",
+	// 	emblemLink: "../atalanta-fugiens/frontispiece.html",
+	// }
+	// var imageItemTemplate = "<div class="image-results__item item--active" data-item-num={{imageArrayNum}}><img class="image-results__image image--active" src={{imageSrc}} /><div class="image-results__details"><div class="image-results__label">{{imageLabel}}</div><div class="image-results__button-row"><button class="image-results__button results__button--terms"><span class="label">Terms</span><span class="icon">X</span></button><a class="image-results__button results__button--edition" href={{emblemLink}}><span class="label">Emblem</span><span class="icon"></span></a><button class="image-results__button results__button--collections"><span class="label">Collection</span><span class="icon"></span></button></div><div class="image-results__all-terms"></div></div></div>"; 
 	var imageCategoryTrigger = '.image-search__category > a';
 	var imageCategoryActive = 'category--active';
+	var imageResultsContainer = '.image-results__container';
 	var resultsTermsTrigger = '.image-results__button-row button:nth-child(1)';
 	var resultsTermsEscapeX = 'image-results__button--open' 
 	var resultsTermsContainer = '.image-results__all-terms';
@@ -9,6 +19,7 @@ $(function () {
 	var resultsTermsRevealed = 'all-terms--active';
 
 /* EVENTS */
+	// $(imageResultsContainer).html(html);
 	$(imageCategoryTrigger).click(function() {
 		var that = this; // store which category link was clicked
 		checkCategorySelected(that);
