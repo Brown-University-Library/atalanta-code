74a2cec5899bcc59d778b6c92aa252507cc30cee
cbrusch <crystal_brusch@brown.edu>
2019-05-02 17:27:06 -0400

    using more reliable method to read dzi files from json

--- dev/atl-output/app/assets/js/zooming-image-viewer--openseadragon.js
+++ dev/atl-output/app/assets/js/zooming-image-viewer--openseadragon.js
@@ -3,32 +3,22 @@ $(function () {
 	var zoomingViewer
 	var myEmblemDataNum = $('.emblem-page').data("id"); // get the data ID for the current emblem page
 	var startPage // the number of first page of current emblem
-	// var pageTiles = "../data/json/page-view.json"; // file path to page view dzi files
-	// var bookTiles = "../data/json/book-view.json"; // file path to book view dzi files
 	var pageTiles = "../data/json/pageView.json"; // file path to page view dzi files
 	var bookTiles = "../data/json/bookView.json"; // file path to book view dzi files
-	var pageView = $.getJSON(pageTiles, function(myJSON) { // get pageView.json file
-		pageView = pageView.responseJSON; // get array of page view URLS
-	})
-	var bookView = $.getJSON(bookTiles, function() { // get bookView.json file
-		bookView = bookView.responseJSON; // get array of book view URLS
-	})
-	.done(function() { // after all the image tiles are ready, display zoomable pages
-		if (myEmblemDataNum <= 3) { // handle front matter (not in sets of 4)
-			startPage = myEmblemDataNum + 7;
-		}
-		else if (myEmblemDataNum === 4) { // handle front matter: preface
-			startPage = myEmblemDataNum + 9;
-		}
-		else if (myEmblemDataNum > 4) { // handle emblem pages in sets of 4
-			startPage = myEmblemDataNum * 4;
-		}
-		// // should the book end at the last page or loop back to the beginning?
 
-		var atalantaZoom = pageView; // current view mode (initially pageView)
+	$.get(pageTiles, function(data) { // after all the image tiles are ready, display zoomable pages
+		if (myEmblemDataNum <= 3) { // handle front matter (not in sets of 4)
+	      startPage = myEmblemDataNum + 7;
+	    }
+	    else if (myEmblemDataNum === 4) { // handle front matter: preface
+	      startPage = myEmblemDataNum + 9;
+	    }
+	    else if (myEmblemDataNum > 4) { // handle emblem pages in sets of 4
+	      startPage = myEmblemDataNum * 4;
+	    }
 		zoomingViewer = OpenSeadragon({
 			id: "openseadragon-wrapper",
-			tileSources: [atalantaZoom],
+			tileSources: data,
 			initialPage: startPage, // start viewer at first page of current emblem
 			autoResize: true, /***/
             showHomeControl: false, /***/
@@ -45,10 +35,8 @@ $(function () {
 			previousButton: "previous",
 			nextButton: "next"
 		});
-		
-		// var pageIndex = this.pageIndex - (this.mode === 'book' ? 2 : 1);
-		// 	if (this.mode === 'book')
 	});
+
 
 	/*** MOTTO WAYPOINT ***/
 	// instantiate the global Waypoint class and pass an options object to it. the two paramaters required are element and handler
