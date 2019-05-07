0a5ffb50f67b110b09eb446f41a3437cae6076ef
cbrusch <crystal_brusch@brown.edu>
2019-05-07 16:09:53 -0400

    adding v3 gulpfile

--- dev/atl-output/gulpfile.js
+++ dev/atl-output/gulpfile.js
@@ -1,43 +1,32 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
-gulp.task('browserSync', function(done) {
+gulp.task('sass', function() {
+	return gulp.src('app/assets/scss/app.scss')
+		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 5 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('app/assets/css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
+gulp.task('browserSync', function() {
 	browserSync.init({
 		server: {
 			baseDir: 'app'
 		},
 		online: true
 	});
-	done();
 });
 
-gulp.task('browserSyncReload', function(done) {
-	browserSync.reload();
-	done();
-});
-
-gulp.task('css', function() {
-	return gulp
-	.src('app/assets/scss/app.scss')
-	.pipe(sass())
-	.pipe(autoprefixer({
-		browsers: ['last 5 versions'],
-		cascade: false
-	}))
-	.pipe(gulp.dest('app/assets/css'))
-	.pipe(browserSync.reload({
-		stream: true
-	}));
-});
-gulp.task('watchFiles', function() {
-	gulp.watch('app/assets/scss/**/*', gulp.series('css'));
-	gulp.watch('app/assets/js/**/*', gulp.series('browserSyncReload'));
-	gulp.watch('app/*.html', gulp.series('browserSyncReload'));
-});
-
-
-gulp.task('watch', gulp.parallel('watchFiles', 'browserSync'), function() {
-
+gulp.task('watch', ['browserSync', 'sass'], function() {
+	gulp.watch('app/assets/scss/**/*.scss', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/assets/js/**/*.js', browserSync.reload);
 });
